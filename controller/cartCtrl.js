43e259464772a742e4baa00918e9249dcffbea82
unjong0926 <153203963+unjong0926@users.noreply.github.com>
2024-08-07 21:22:38 +0900

    장바구니 컨트롤러 get 오류 수정

--- controller/cartCtrl.js
+++ controller/cartCtrl.js
@@ -4,9 +4,14 @@ const pool = require('../DB/db');
 exports.getCart = async (req, res) => {
   try{
     const user = req.session.user;
-    const searchCartQ = 'SELECT cart_id FROM cart WHERE user_user_id = ?'
-    const searchCartR = await pool.query(searchCartQ,[user])
-    const userCartId = searchCartR[0][0].cart_id
+    console.log(user, "장바구니 페이지") //세션 체크용 콘솔
+    if(!user){
+      console.log("아직 로그인하지 않았습니다.")
+    }
+    else{
+          const searchCartQ = 'SELECT cart_id FROM cart WHERE user_user_id = ?'
+          const searchCartR = await pool.query(searchCartQ,[user])
+        const userCartId = searchCartR[0][0].cart_id
     const showCartListQ = `
       SELECT book_info.book_name, book_info.book_inventory, book_info.book_price, cart_list.count_book
       FROM book_info
@@ -14,8 +19,9 @@ exports.getCart = async (req, res) => {
       WHERE cart_list.cart_cart_id = ?;
     `;
     const showCartListR = await pool.query(showCartListQ,[userCartId]);
-
+    console.log(showCartListR[0]) // 에러 체크용 콘솔
     res.send(showCartListR[0])
+    }
   }
   catch(err){
     console.error(err)
